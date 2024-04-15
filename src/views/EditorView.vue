1ba598157c97a55638e05b09022fa0bf5048e916
R10K5 <r10k5.job@gmail.com>
2024-04-15 16:26:58 +0300

    faet(editor): create editor view;

---
+++ src/views/EditorView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+<div class="editor-container">
+  <h1>
+    Добавление продукта:
+  </h1>
+</div>
+</template>
+
+<style>
+.editor-container {
+  width: 100%;
+}
+</style>
