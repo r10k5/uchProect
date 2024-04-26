21e32e2b5fd11fe3f09453eb3266f8a72e71cf8a
r10k5 <r10k5.job@gmail.com>
2024-04-26 22:03:32 +0300

    feat(category-list): add check cataloge type?

--- src/components/AppCategoryList.vue
+++ src/components/AppCategoryList.vue
@@ -3,15 +3,27 @@ import { useCatalogeStore } from '@/stores/category.store';
 import { computed } from 'vue';
 
 const catalogStore = useCatalogeStore();
-const catalogs = computed( () => catalogStore.cataloges)
+const catalogs = computed(() => catalogStore.cataloges);
+const rootCatalogs = computed(
+    () => catalogStore.cataloges.filter((catalog) => catalog.parent_id === null)
+);
+
+const getSubCategories = (categoryId: number) => {
+    return catalogs.value.filter((cataloge) => cataloge.parent_id === categoryId);
+}
 </script>
 
 <template>
    <router-link
     to="/edit"
     class="category-list-container">
-        <div v-for="cataloge in catalogs" :key="cataloge.id" class="">
-            {{cataloge.name}}
+        <div v-for="cataloge in rootCatalogs" :key="cataloge.id" class="category-list-container__text">
+            <span>{{cataloge.name}}</span>
+            <div class="category-list-container__sub-category">
+                <span v-for="category in getSubCategories(cataloge.id)" :key="category.id">
+                    {{ category.name }}
+                </span>
+            </div>
         </div>
     </router-link>
 </template>
@@ -19,5 +31,15 @@ const catalogs = computed( () => catalogStore.cataloges)
 <style scoped setup lang="scss">
 .category-list-container {
     background-color: rgb(200, 156, 156);
+    &__text {
+        font-size: 20px;
+    }
+    &__sub-category {
+        display: flex;
+        flex-direction: column;
+        gap: 4px;
+        padding-left: 1rem;
+        font-size: 16px;
+    }
 }
 </style>
