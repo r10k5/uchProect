3e483b3d72936e8f7dbd4c899cb8d3cd6b23fe5d
r10k5 <r10k5.job@gmail.com>
2024-04-26 17:55:56 +0300

    feat(save-button): now can add new card;

--- src/views/EditorView.vue
+++ src/views/EditorView.vue
@@ -1,17 +1,42 @@
 <script setup lang="ts">
+import type { Product } from '@/services/types/cards.types';
+import { useCardsStore } from '@/stores/cards.store';
 import { ref } from 'vue';
+
 type FieldType = {
   fieldType: 'number' | 'string',
   value: string,
   name: string,
 }
+
 const fields = ref<FieldType[]>([]);
+
+const description = ref('');
+const photo = ref('');
+const article = ref('');
+const name = ref('');
+const price = ref(0);
+
+
 const addField = () => {
   fields.value.push({
     fieldType: 'number',
     value: '',
     name: '',
   })
+}
+
+const cardsStore = useCardsStore();
+
+const addNewCard = () => {
+  cardsStore.addCardLocal({
+    id: Math.random(),
+    name: name.value,
+    price: price.value,
+    article: article.value,
+    photo: photo.value,
+    description: description.value,
+  });
 }
 </script>
 
@@ -20,35 +45,35 @@ const addField = () => {
   <h1 class="editor-container__header">
     Добавление продукта
   </h1>
-  <form class="form-container">
+  <div class="form-container">
     <div class="form-container__field">
       <lable for="name">Название:</lable>
-      <input id="name"/>
+      <input id="name" v-model="name" />
     </div>
 
     <div class="form-container__field">
       <lable for="category">Категория:</lable>
-      <input id="category"/>
+      <input id="category" />
     </div>
     
     <div class="form-container__field">
       <lable for="description">Описание:</lable>
-      <input id="description"/>
+      <input id="description" v-model="description"/>
     </div>
 
     <div class="form-container__field">
       <lable for="price">Стоимость:</lable>
-      <input id="price"/>
+      <input id="price" v-model="price"/>
     </div>
     
     <div class="form-container__field">
       <lable for="photo">Загрузить изображение</lable>
-      <input id="photo"/>
+      <input id="photo" v-model="photo"/>
     </div>
     
     <div class="form-container__field">
       <lable for="article">Артикул:</lable>
-      <input id="article"/>
+      <input id="article" v-model="article"/>
     </div>
 
     <div v-for="(field, index) in fields" :key="index" class="form-container__field">
@@ -61,8 +86,8 @@ const addField = () => {
     </div>
 
     <button type="button" @click="addField">Добавить поле</button>
-    <button>Сохранить</button>
-  </form>
+    <button @click="addNewCard">Сохранить</button>
+  </div>
 </div>
 </template>
 
